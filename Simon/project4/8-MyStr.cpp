0405f41bb89d0fbd1dd50a22f8624923fe33658a
simon-techkid <simon.techkid@outlook.com>
2025-10-10 00:41:29 -0700

    Project 4 Largely Completed

--- Simon/project4/8-MyStr.cpp
+++ Simon/project4/8-MyStr.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
+#define NULL_TERMINATOR '\0'
+
 #include "8-MyStr.h"
 
-//Constructor
 MyString::MyString(char const* strIn) {
     int length = myStrLen(strIn);
     str = new char[length + 1];
@@ -12,10 +13,9 @@ MyString::MyString(char const* strIn) {
         str[i] = strIn[i];
         i++;
     }
-    str[i] = '\0';
+    str[i] = NULL_TERMINATOR;
 }
 
-//Destructor function
 MyString::~MyString() {
     delete []str; 
 }
@@ -25,12 +25,113 @@ void MyString::myDisplay() {
 }
 
 int MyString::myStrLen(char const* strIn) {
+    if (strIn == nullptr) return 0;
+
     int idx = 0;
-    while (strIn[idx] != '\0')
+    while (strIn[idx] != NULL_TERMINATOR)
     idx++;
     return idx;
 }
 
 int MyString::myStrlen() {
     return myStrLen(str);
+}
+
+void MyString::myStrcpy(char const* strIn) {
+    if (strIn == nullptr) return;
+    if (strIn == str) return;
+
+    int lenIn = myStrLen(strIn);
+    char* newStr = new char[lenIn + 1];
+    
+    int index = 0;
+    while (index < lenIn) {
+        newStr[index] = strIn[index];
+        index++;
+    }
+    newStr[index] = NULL_TERMINATOR;
+
+    char* oldStr = str;
+    str = newStr;
+    delete []oldStr;
+}
+
+bool MyString::isEqual(char const* strIn) {
+    if (strIn == nullptr) return false;
+    if (strIn == str) return true;
+
+    int lenThis = myStrlen();
+    int lenThat = myStrLen(strIn);
+    if (lenThis != lenThat) return false;
+
+    for (int i = 0; i < lenThis; i++) {
+        if (str[i] != strIn[i]) return false;
+    }
+
+    return true;
+}
+
+bool MyString::isSub(char const* strIn, int idx) {
+    if (strIn == nullptr) return false;
+    if (strIn == str) return true;
+
+    int lenThis = myStrlen();
+    int lenThat = myStrLen(strIn);
+    int adjustment = idx + lenThat;
+    if (adjustment > lenThis) return false;
+
+    for (
+        int idxThis = idx, idxThat = 0;
+        idxThat < lenThat;
+        idxThis++, idxThat++) {
+        if (str[idxThis] != strIn[idxThat]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int MyString::find(char const* strIn) {
+    if (strIn == nullptr) return -1;
+    if (myStrLen(strIn) == 0) return 0;
+
+    int lenThis = myStrlen();
+    int lenThat = myStrLen(strIn);
+    if (lenThat == 0) return -1;
+
+    for (int i = 0; i < lenThis; i++) {
+        if (str[i] == strIn[0]) {
+            if (isSub(strIn, i)) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+void MyString::concat(char const* strIn) {
+    if (strIn == nullptr) return;
+
+    int lenThis = myStrlen();
+    int lenThat = myStrLen(strIn);
+    if (lenThat == 0) return;
+
+    int totalLength = lenThis + lenThat + 1;
+    char* newArr = new char[totalLength];
+
+    for (int i = 0; i < lenThis; i++) {
+        newArr[i] = str[i];
+    }
+
+    for (int i = 0; i < lenThat; i++) {
+        newArr[lenThis + i] = strIn[i];
+    }
+
+    newArr[lenThis + lenThat] = NULL_TERMINATOR;
+
+    char* tmp = str;
+    str = newArr;
+    delete tmp;
 }
--- Simon/project4/8-MyStrTst.cpp
+++ Simon/project4/8-MyStrTst.cpp
@@ -7,7 +7,7 @@ Description: Program illustrates using class MyString
 */
 
 #include "8-MyStr.h"
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 //usage ./a.out Higgledy
@@ -31,6 +31,49 @@ int main(int argc, char* argv[]) {
     cout << str2->myStrlen() << endl;
     cout << endl;
     //End Test of myStrlen
+
+    //Test of myStrcpy
+    cout << "*****Test myStrcpy*****" << endl;
+    str2->myDisplay();
+    char testStr1[6] = "Hello";
+    str2->myStrcpy(testStr1);
+    str2->myDisplay();
+    char testStr2[8] = "Goodbye";
+    str2->myStrcpy(testStr2);
+    str2->myDisplay();
+    cout << endl;
+    //End Test of myStrcpy
+    
+    //Test of isEqual
+    cout << "*****Test isEqual*****" << endl;
+    MyString isEq("hi");
+    bool isEqTest1 = !isEq.isEqual("Hi");
+    isEq.myStrcpy("Hi");
+    bool isEqTest2 = isEq.isEqual("Hi");
+    cout << "isEqual Pass: " << (isEqTest1 && isEqTest2) << endl;
+    cout << endl;
+    //End Test of myStrcpy
+
+    //Test of find
+    cout << "*****Test find*****" << endl;
+    MyString find("The Quick Brown Fox Jumped Over the Lazy Dog");
+    bool test1Pass = find.find("Quick") == 4;
+    bool test2Pass = find.find("Fox") == 16;
+    bool test3Pass = find.find("Dog") == 41;
+    bool test4Pass = find.find("Hello") == -1;
+    cout << "find Pass: " << (test1Pass && test2Pass && test3Pass && test4Pass) << endl;
+    cout << endl;
+    //End Test of myStrcpy
+
+    //Test of concat
+    cout << "*****Test concat*****" << endl;
+    MyString concat("Hello");
+    concat.myDisplay();
+    concat.concat("Simon");
+    concat.myDisplay();
+    cout << concat.myStrlen();
+    cout << endl;
+    //End Test of concat
 
     delete str2;
     return 0;  
