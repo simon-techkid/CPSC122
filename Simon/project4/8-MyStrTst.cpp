ff13f366a334e08b100d73f044c3b0ada74c20c0
simon-techkid <simon.techkid@outlook.com>
2025-10-10 01:43:41 -0700

    Project 4 Tests WIP

--- Simon/project4/8-MyStrTst.cpp
+++ Simon/project4/8-MyStrTst.cpp
@@ -13,8 +13,13 @@ using namespace std;
 //usage ./a.out Higgledy
 //where "Hiddledy" is input string
 int main(int argc, char* argv[]) {
-    MyString str1(argv[1]);
-    MyString* str2 = new MyString(argv[1]);
+    if (argc != 2) {
+        cout << "Incorrect number of command line arguments" << endl;
+        return 1;
+    }
+    char* argument = argv[1];
+    MyString str1(argument);
+    MyString* str2 = new MyString(argument);
 
     //Test of myDisplay
     cout << "*****Test myDisplay*****" << endl;
@@ -34,11 +39,14 @@ int main(int argc, char* argv[]) {
 
     //Test of myStrcpy
     cout << "*****Test myStrcpy*****" << endl;
+    cout << "myStrcpy Initial Value (\"" << argument << "\"): ";
     str2->myDisplay();
     char testStr1[6] = "Hello";
     str2->myStrcpy(testStr1);
+    cout << "myStrcpy Run (\"Hello\"): ";
     str2->myDisplay();
     char testStr2[8] = "Goodbye";
+    cout << "myStrcpy Run (\"Goodbye\"): ";
     str2->myStrcpy(testStr2);
     str2->myDisplay();
     cout << endl;
@@ -50,28 +58,48 @@ int main(int argc, char* argv[]) {
     bool isEqTest1 = !isEq.isEqual("Hi");
     isEq.myStrcpy("Hi");
     bool isEqTest2 = isEq.isEqual("Hi");
-    cout << "isEqual Pass: " << (isEqTest1 && isEqTest2) << endl;
+    cout << "isEqual Run: " << ((isEqTest1 && isEqTest2) ? "yes" : "no") << endl;
     cout << endl;
     //End Test of myStrcpy
 
     //Test of find
     cout << "*****Test find*****" << endl;
     MyString find("The Quick Brown Fox Jumped Over the Lazy Dog");
-    bool test1Pass = find.find("Quick") == 4;
-    bool test2Pass = find.find("Fox") == 16;
-    bool test3Pass = find.find("Dog") == 41;
-    bool test4Pass = find.find("Hello") == -1;
-    cout << "find Pass: " << (test1Pass && test2Pass && test3Pass && test4Pass) << endl;
+    char testFind1[6] = "Quick";
+    char testFind2[4] = "Fox";
+    char testFind3[4] = "Dog";
+    char testFind4[6] = "Hello";
+    bool test1Pass = find.find(testFind1) == 4;
+    bool test2Pass = find.find(testFind2) == 16;
+    bool test3Pass = find.find(testFind3) == 41;
+    bool test4Pass = find.find(testFind4) == -1;
+    bool allPass = test1Pass && test2Pass && test3Pass && test4Pass;
+    cout << "find Run: " << (allPass ? "yes" : "no") << endl;
     cout << endl;
     //End Test of myStrcpy
 
     //Test of concat
     cout << "*****Test concat*****" << endl;
-    MyString concat("Hello");
-    concat.myDisplay();
-    concat.concat("Simon");
-    concat.myDisplay();
-    cout << concat.myStrlen();
+    char testConcat1[6] = "Hello";
+    char testConcat2[6] = "World";
+    char testConcat3[9] = "Goodbyte";
+    char testConcat4[6] = "World";
+    MyString testConcatStr(testConcat1);
+    cout << "concat Initial Value (\"Hello\"): ";
+    testConcatStr.myDisplay();
+
+    testConcatStr.concat(testConcat2);
+    cout << "concat Run (\"" << testConcat2 << "\"): ";
+    testConcatStr.myDisplay();
+    
+    testConcatStr.concat(testConcat3);
+    cout << "concat Run (\"" << testConcat3 << "\"): ";
+    testConcatStr.myDisplay();
+
+    testConcatStr.concat(testConcat4);
+    cout << "concat Run (\"" << testConcat4 << "\"): ";
+    testConcatStr.myDisplay();
+
     cout << endl;
     //End Test of concat
 
