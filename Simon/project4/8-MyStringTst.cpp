996ffc705c5e2d72143537a22d40b9e303b06f27
simon-techkid <simon.techkid@outlook.com>
2025-10-10 18:44:25 -0700

    Project 4 -- Finalize Tests

--- Simon/project4/8-MyStringTst.cpp
+++ Simon/project4/8-MyStringTst.cpp
@@ -41,31 +41,40 @@ int main(int argc, char* argv[]) {
 
     //Test of myStrcpy
     cout << "*****Test myStrcpy*****" << endl;
-    cout << "myStrcpy Initial Value (\"" << argument << "\"): ";
+    cout << "dynamic test" << endl;
+    cout << "myStrcpy Initial Value (Expecting \"" << argument << "\"): ";
     str2->myDisplay();
+
     char testStr1[6] = "Hello";
     str2->myStrcpy(testStr1);
-    cout << "myStrcpy Run (\"Hello\"): ";
+    cout << "myStrcpy Run (Expecting \"Hello\"): ";
     str2->myDisplay();
+    
     char testStr2[8] = "Goodbye";
-    cout << "myStrcpy Run (\"Goodbye\"): ";
+    cout << "myStrcpy Run (Expecting \"Goodbye\"): ";
     str2->myStrcpy(testStr2);
     str2->myDisplay();
+    
     cout << endl;
     //End Test of myStrcpy
     
     //Test of isEqual
     cout << "*****Test isEqual*****" << endl;
+    cout << "static test" << endl;
+
     MyString isEq("hi");
     bool isEqTest1 = !isEq.isEqual("Hi");
+    
     isEq.myStrcpy("Hi");
     bool isEqTest2 = isEq.isEqual("Hi");
-    cout << "isEqual Run: " << ((isEqTest1 && isEqTest2) ? "yes" : "no") << endl;
+    
+    cout << "isEqual Pass: " << ((isEqTest1 && isEqTest2) ? "yes" : "no") << endl;
     cout << endl;
     //End Test of myStrcpy
 
     //Test of find
     cout << "*****Test find*****" << endl;
+    cout << "static test" << endl;
     MyString find("The Quick Brown Fox Jumped Over the Lazy Dog");
     char testFind1[6] = "Quick";
     char testFind2[4] = "Fox";
@@ -76,30 +85,32 @@ int main(int argc, char* argv[]) {
     bool test3Pass = find.find(testFind3) == 41;
     bool test4Pass = find.find(testFind4) == -1;
     bool allPass = test1Pass && test2Pass && test3Pass && test4Pass;
-    cout << "find Run: " << (allPass ? "yes" : "no") << endl;
+    cout << "find Pass: " << (allPass ? "yes" : "no") << endl;
     cout << endl;
     //End Test of myStrcpy
 
     //Test of concat
     cout << "*****Test concat*****" << endl;
+    cout << "static test" << endl;
     char testConcat1[6] = "Hello";
     char testConcat2[6] = "World";
     char testConcat3[9] = "Goodbyte";
     char testConcat4[6] = "World";
+    
     MyString testConcatStr(testConcat1);
-    cout << "concat Initial Value (\"Hello\"): ";
+    cout << "concat Initial Value (Expecting \"Hello\"): ";
     testConcatStr.myDisplay();
 
     testConcatStr.concat(testConcat2);
-    cout << "concat Run (\"" << testConcat2 << "\"): ";
+    cout << "concat Run (Added \"" << testConcat2 << "\"): ";
     testConcatStr.myDisplay();
     
     testConcatStr.concat(testConcat3);
-    cout << "concat Run (\"" << testConcat3 << "\"): ";
+    cout << "concat Run (Added \"" << testConcat3 << "\"): ";
     testConcatStr.myDisplay();
 
     testConcatStr.concat(testConcat4);
-    cout << "concat Run (\"" << testConcat4 << "\"): ";
+    cout << "concat Run (Added \"" << testConcat4 << "\"): ";
     testConcatStr.myDisplay();
 
     cout << endl;
